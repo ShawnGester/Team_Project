87345d8fea723875b90c342f5636a6e0da80ab6d
ShawnGester <sge23@wisc.edu>
2018-11-27 20:02:54 -0600

    Completed left flowpane of primary GUI

--- PrimaryGUI.java
+++ PrimaryGUI.java
@@ -1,12 +1,17 @@
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
@@ -21,34 +26,19 @@ public class PrimaryGUI {
 	public PrimaryGUI(FoodData foodData, Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,1400,800);
+			Scene scene = new Scene(root,1400,900);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm()); //FIXME: Alex's
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
-//			Label shawn = new Label("Shawn Ge");
-//			Label cole = new Label("Cole Thomson");
-//			Label dani = new Label("Danica Fliss");
-//			Label leah = new Label("Leah Witt");
-//			Label alex = new Label("Alexander Fusco");
-//			HBox names = new HBox(200.0, shawn, cole, dani, leah, alex); //FIXME: ?
-			
-			Button newFood = new Button("+New Food");
-			Button newMeal = new Button("+New Meal");
+			Button newFood = new Button("+ New Food");
+			Button newMeal = new Button("+ New Meal");
 			Label topLabel = new Label("Meal Planner");
 			
 			Label centerLabel = new Label("Details");
-//			centerLabel.setFont(new Font("Arial", 30));
-			
-			
-//			BorderPane left = new BorderPane(newFood);
-			BorderPane right = new BorderPane(newMeal);
-//			BorderPane bottom = new BorderPane(names);
 			BorderPane top = new BorderPane(topLabel);
 			BorderPane center = new BorderPane(centerLabel);
 			
-//			root.setLeft(test);
-			root.setRight(right);
 			root.setTop(top);
 //			root.setBottom(bottom);
 			root.setCenter(center);
@@ -56,11 +46,54 @@ public class PrimaryGUI {
 			
 			FlowPane flow = new FlowPane(Orientation.VERTICAL, 0, 10); //gives vertical spacing between 
 			flow.setAlignment(Pos.TOP_LEFT); //left aligns all members of flow pane
-			flow.setPadding(new Insets(0,0,0,10));
+			flow.setPadding(new Insets(0,20,0,10));
 			root.setLeft(flow);
 			flow.setColumnHalignment(HPos.LEFT); // align labels on left
-			flow.getChildren().add(newFood);
-			flow.getChildren().add(newMeal);
+			TextField queryFood = new TextField("Search for a food...");
+			
+			ListView<String> foodList = new ListView<String>();
+			foodList.setPrefWidth(300);
+			foodList.setPrefHeight(300);
+			//FIXME: event handler to foods
+			Label selectedFoods = new Label("Displaying 0 of 0 foods"); //number of foods out of total after filter
+			
+			//FILTERGUI START
+			ObservableList<String> filterOptions = FXCollections.observableArrayList(
+					"Calories",
+					"Protein",
+					"Fat"
+				); //FIXME:
+			ComboBox filters = new ComboBox(filterOptions);
+			filters.setValue("Select a filter");
+			ObservableList<String> compOptions = FXCollections.observableArrayList(
+					"=",
+					">=",
+					"<="
+				); //FIXME:
+			ComboBox compFilters = new ComboBox(compOptions);
+			compFilters.setValue("=");
+			TextField queryValue = new TextField("");
+			Button add = new Button("Add");
+			HBox filter = new HBox(10);
+			filter.getChildren().addAll(compFilters, queryValue, add); //FIXME: queryValue and add not staying on same line??
+			ListView<String> filterList = new ListView<String>();
+			filterList.setPrefWidth(200);
+			filterList.setPrefHeight(150);
+			HBox buttons = new HBox(20);
+			Button edit = new Button("EDIT");
+			Button delete = new Button("DELETE");
+			buttons.getChildren().addAll(edit, delete);
+			//FILTERGUI END
+			
+			Button displayFood = new Button("Display Food");
+			Button downloadFood = new Button("Download Food List");
+			Label foods = new Label("Foods");
+			
+//			ObservableList<String> items = FXCollections.observableArrayList (); //MILESTONE 3 READ IN FROM FOODDATA
+//			List.setItems(items);
+			flow.getChildren().addAll(newFood, queryFood, foodList, selectedFoods, filters, 
+					compFilters, filter, filterList, buttons, displayFood, downloadFood, foods);
+			
 			
 		} catch(Exception e) {
 			e.printStackTrace();
