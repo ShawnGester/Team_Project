9d61300e4a783c9f9df6291dc09a8fc333bb8b65
flissd <dfliss@wisc.edu>
2018-12-10 10:46:59 -0600

    Rough outline of necessary pieces

--- application/PopUpFood.java
+++ application/PopUpFood.java
@@ -32,41 +32,93 @@ public class PopUpFood {
 	
 	public PopUpFood(){
 		foodWindow = new Stage();
+		foodWindow.setTitle("Create A Food Item");
 		BorderPane food = new BorderPane();
 		
+		//top of the pop up window
 		Label foodTitle = new Label("Create Food!");
 		foodTitle.setFont(new Font(24));
+		HBox top = new HBox(10);
+		top.getChildren().add(foodTitle);
+//		food.setTop(top);
+		BorderPane.setAlignment(top, Pos.CENTER);
 		
+		//Left side of pop up window
 		Label upload = new Label("Upload New Food File");
+		upload.setFont(new Font(20));
 		
 		TextField filePath = new TextField();
 		Label fileLabel = new Label("Input Filename");
 		
 		Button uploadButton = new Button("Upload");
 		
+		BorderPane left = new BorderPane();
+		VBox text = new VBox(10);
+		text.getChildren().addAll(fileLabel, filePath, uploadButton);
+		left.setTop(upload);
+		left.setCenter(text);
+		
+		food.setLeft(left);
+		food.setAlignment(left, Pos.CENTER_RIGHT);
 		
 		Label newFood = new Label("Add New Food to List");
+		newFood.setFont(new Font(20));
 		
 		Label foodName = new Label("Name of Food");
 		TextField foodInput = new TextField("e.g. \"Apple\"");
 		foodInput.setFocusTraversable(false);
 		
+		VBox nameInfo = new VBox(10);
+		nameInfo.getChildren().addAll(foodName, foodInput);
+		
 		Label calories = new Label("calories");
 		TextField calorieInput = new TextField();
+		
+//		HBox calInfo = new HBox(10);
+//		calInfo.getChildren().addAll(calories, calorieInput);
 		
 		Label protein = new Label("protein in grams");
 		TextField proteinInput = new TextField();
 		
+//		HBox proteinInfo = new HBox(10);
+//		proteinInfo.getChildren().addAll(protein, proteinInput);
+		
 		Label carb = new Label("carbohydrates in grams");
 		TextField carbInput = new TextField();
+		
+//		HBox carbInfo = new HBox(10);
+//		carbInfo.getChildren().addAll(carb, carbInput);
 		
 		Label fat = new Label("fat in grams");
 		TextField fatInput = new TextField();
 		
+//		HBox fatInfo = new HBox(10);
+//		fatInfo.getChildren().addAll(fat, fatInput);
+		
 		Label fiber = new Label("fiber in grams");
 		TextField fiberInput = new TextField();
 		
+//		HBox fiberInfo = new HBox(10);
+//		fiberInfo.getChildren().addAll(fiber, fiberInput);
+		
 		Button addFood = new Button("Add Food");
+		
+		VBox right = new VBox(10);		
+		VBox labels = new VBox(20);
+		labels.getChildren().addAll(calories, protein, carb, fat, fiber);
+		
+		VBox fields = new VBox(10);
+		fields.getChildren().addAll(calorieInput, proteinInput, carbInput, fatInput, fiberInput);
+		
+		HBox nutrientBox = new HBox(10);
+		nutrientBox.getChildren().addAll(labels, fields);
+		
+		right.getChildren().addAll(newFood, nameInfo, nutrientBox, addFood);
+		food.setRight(right);
+		
+		Scene foodScene = new Scene(food, 600, 400);
+		foodWindow.setScene(foodScene);
+		foodWindow.show();
 		
 	}
 
