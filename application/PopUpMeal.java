5d22ea3374e765c7c5c767b68276ff1dd38b09ba
flissd <dfliss@wisc.edu>
2018-12-10 09:44:53 -0600

    Edited PopUpMeal to look a bit better

--- application/PopUpMeal.java
+++ application/PopUpMeal.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
@@ -29,39 +31,83 @@ public class PopUpMeal{
 	//Popup meal = new Popup();
 	Stage mealWindow;
 	
-	public PopUpMeal(){
+	public PopUpMeal(List<FoodItem> food){
+//		mealWindow = new Stage();
+//		VBox left = new VBox(30);
+//		VBox right = new VBox(10);
+//		VBox mealInfoBox = new VBox(10);
+//		HBox buttonBox = new HBox(10);
+//		HBox window = new HBox(10);
+//		
+//		Label mealEntry = new Label("Enter Name of Meal");
+//		TextField mealNameBox = new TextField();
+//		mealNameBox.setPromptText("e.g. \"Breakfast\"");
+//		mealNameBox.setFocusTraversable(false);
+//		Button addMealButton = new Button("Add Meal");
+//		
+//		mealInfoBox.getChildren().addAll(mealEntry, mealNameBox);
+//		buttonBox.getChildren().add(addMealButton);
+//		buttonBox.setAlignment(Pos.CENTER);
+//		
+//		left.getChildren().addAll(mealInfoBox, buttonBox);
+//		
+//		window.getChildren().addAll(left, right);
+//		window.setAlignment(Pos.BOTTOM_CENTER);
+//		window.setPrefWidth(600);
+//		window.setPrefHeight(400);
+//		
+//		window.setStyle("-fx-background-color: aquamarine");
+//		
+//		Scene mealScene = new Scene(window, 600, 400);
+//		
+//		
+//		mealWindow.setScene(mealScene);
+//		mealWindow.show();
+		
+		
 		mealWindow = new Stage();
-		VBox left = new VBox(30);
+		
+		BorderPane mealBorder = new BorderPane();
+		HBox title = new HBox(10);
+		VBox left = new VBox(10);
 		VBox right = new VBox(10);
-		VBox mealInfoBox = new VBox(10);
 		HBox buttonBox = new HBox(10);
-		HBox window = new HBox(10);
 		
-		Label mealEntry = new Label("Enter Name of Meal");
-		TextField mealNameBox = new TextField();
-		mealNameBox.setPromptText("e.g. \"Breakfast\"");
-		mealNameBox.setFocusTraversable(false);
-		Button addMealButton = new Button("Add Meal");
+		Label header = new Label("Create Your Meal!");
+		header.setFont(new Font(25));
 		
-		mealInfoBox.getChildren().addAll(mealEntry, mealNameBox);
-		buttonBox.getChildren().add(addMealButton);
+		title.getChildren().add(header);
+		title.setAlignment(Pos.CENTER);
+		
+		Label namePrompt = new Label("Enter Name of Meal");
+		TextField nameInput = new TextField();
+		nameInput.setPromptText("e.g. \"Breakfast\"");
+		nameInput.setFocusTraversable(false);
+		
+		left.getChildren().addAll(namePrompt, nameInput);
+		left.setAlignment(Pos.CENTER);
+		
+		Label foodLabel = new Label("Choose the food to include in you meal");
+		ScrollPane foodList = new ScrollPane();
+		foodList.setMinHeight(200);
+		
+		right.getChildren().addAll(foodLabel, foodList);
+		right.setAlignment(Pos.CENTER);
+		
+		Button addMeal = new Button("Add Meal");
+		
+		buttonBox.getChildren().add(addMeal);
 		buttonBox.setAlignment(Pos.CENTER);
 		
-		left.getChildren().addAll(mealInfoBox, buttonBox);
+		mealBorder.setTop(title);
+		mealBorder.setLeft(left);
+		mealBorder.setRight(right);
+		mealBorder.setBottom(buttonBox);
 		
-		window.getChildren().addAll(left, right);
-		window.setAlignment(Pos.BOTTOM_CENTER);
-		window.setPrefWidth(600);
-		window.setPrefHeight(400);
-		
-		window.setStyle("-fx-background-color: aquamarine");
-		
-		Scene mealScene = new Scene(window, 600, 400);
-		
+		Scene mealScene = new Scene(mealBorder, 600, 400);
 		
 		mealWindow.setScene(mealScene);
 		mealWindow.show();
-		
 	}
 
 }
