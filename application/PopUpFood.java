560c3f6659b6f6da24aa04a7121d8ecbe4abc36d
Cole Thomson <cthomson4@wisc.edu>
2018-12-12 17:36:13 -0600

    New FoodData

--- application/PopUpFood.java
+++ application/PopUpFood.java
@@ -275,7 +275,7 @@ public class PopUpFood {
 	/**
 	 * checks to see if input value is a valid input. If there is no
 	 * input, the value is automatically set to 0.0. Throws exception
-	 * if parameter value cannot be parsed into a Double or is negative.
+	 * if parameter value cannot be parsed into a Double.
 	 * @param String
 	 * @return
 	 */
@@ -283,11 +283,8 @@ public class PopUpFood {
 		if(value == null || value.equals("")) 
 			//if no value specified, automatically set value
 			return 0.0;
-		)else if(Double.parseDouble(value)<0){
-			throw NullPointerException;
-		}else{
+		else
 			return Double.parseDouble(value);
-		}
 	}
 
 }
